e04fdaafcca56aaf9565c0213bdbfbbe51518999
Dibarnita Paul <dibarnita12@gmail.com>
2025-08-29 17:10:02 +0600

    Update and rename Problem 2.c to Problem 2.cpp

---
+++ Problem 2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    
+
+    float pi=3.1416;
+    
+    float tr=45*pi/180;
+
+   
+    float ratio = 1.0 / sin(tr);
+
+    cout << "The ratio of slit width to wavelength is: "<<ratio;
+
+
+}
